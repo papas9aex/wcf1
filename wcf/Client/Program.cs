90b9d47641a4294902a676527ab7dc7d65684db3
sss <137614601+papas9aex@users.noreply.github.com>
2024-11-26 08:23:01 +0300

    Add files via upload

---
+++ wcf/Client/Program.cs
@@ -0,0 +1,79 @@
+﻿using Kt10WcfServiceLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uri uri = new Uri("http://localhost:8733/Design_Time_Addresses/WcfServiceLibrary1/Service1/");
+            //WSHttpBinding binding = new WSHttpBinding(SecurityMode.Message);
+
+            //using (ServiceHost host = new ServiceHost(typeof(Service1), uri))
+            //{
+            //    host.Open();
+            //    Console.WriteLine("Сервис запущен.");
+            //    Console.WriteLine("Нажмите Enter для остановки сервиса…");
+            //    Console.ReadLine();
+            //    string response = host.ToS("Test message");
+
+            //    host.Close();
+            //}
+            //using (ChannelFactory<ISecureService> factory = new ChannelFactory<ISecureService>(binding, uri))
+            //{
+            //    // Установка учетных данных
+            //    factory.Credentials.UserName.UserName = "username";
+            //    factory.Credentials.UserName.Password = "password";
+
+            //    // Создание прокси
+            //    ISecureService proxy = factory.CreateChannel();
+
+            //    // Вызов метода службы
+            //    string response = proxy.SecureMethod("Test message");
+
+            //    Console.WriteLine("Response from service: " + response);
+            //}
+            //EndpointAddress address = new EndpointAddress("http://localhost:8733/Design_Time_Addresses/Kt10WcfServiceLibrary2/Service1/mex");
+
+            //// Привязка WsHttpBinding с безопасностью на уровне сообщений
+            //WSHttpBinding binding = new WSHttpBinding(SecurityMode.Message);
+
+            //// Создание канала к службе
+            //using (ChannelFactory<ISecureService> factory = new ChannelFactory<ISecureService>(binding, address))
+            //{
+            //    // Установка учетных данных
+            //    factory.Credentials.UserName.UserName = "username";
+            //    factory.Credentials.UserName.Password = "password";
+
+            //    // Создание прокси
+            //    ISecureService proxy = factory.CreateChannel();
+
+            //    // Вызов метода службы
+            //    string response = proxy.SecureMethod("Test message");
+
+            //    Console.WriteLine("Response from service: " + response);
+            //}
+            var binding = new WSHttpBinding();
+            binding.Security.Mode = SecurityMode.Message;
+            binding.Security.Message.ClientCredentialType =
+            MessageCredentialType.Windows;
+            var endpoint = new EndpointAddress("http://localhost:8733/Design_Time_Addresses/Kt10WcfServiceLibrary2/Service1/");
+            var channelFactory = new ChannelFactory<ISecureService>(binding, endpoint);
+
+              var proxy = channelFactory.CreateChannel();
+              var s =  proxy.SecureMethod("12");
+            //proxy.SecureMethod();
+            //client.Call Secure Method();
+            Console.WriteLine(s);
+            Console.WriteLine("Press <Enter> to exit.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ wcf/Kt10WcfServiceLibrary2/SecureService.cs
@@ -0,0 +1,138 @@
+﻿using Kt10WcfServiceLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Kt10WcfServiceLibrary2
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Service1 : ISecureService
+    {
+        public string SecureMethod(string input)
+        {
+            return "Response from SecureMethod: " + input;
+        }
+ //       	<appSettings>
+	//	<add key = "aspnet:UseTaskFriendlySynchronizationContext" value="true" />
+	//</appSettings>
+	//<system.web>
+	//	<compilation debug = "true" />
+
+ //   </ system.web >
+
+ //   < system.serviceModel >
+
+ //       < services >
+
+ //           < service name="Kt10WcfServiceLibrary2.Service1">
+	//			<endpoint address = "" binding="wsHttpBinding" contract="Kt10WcfServiceLibrary2.ISecureService">
+	//			</endpoint>
+	//			<endpoint address = "mex" binding="mexHttpBinding" contract="IMetadataExchange" />
+	//			<host>
+	//				<baseAddresses>
+	//					<add baseAddress = "http://localhost:8733/Design_Time_Addresses/Kt10WcfServiceLibrary2/Service1/" />
+
+ //                   </ baseAddresses >
+
+ //               </ host >
+
+ //           </ service >
+
+ //       </ services >
+
+ //       < behaviors >
+
+ //           < serviceBehaviors >
+
+ //               < behavior >
+
+ //                   < serviceMetadata httpGetEnabled="True" httpsGetEnabled="True"/>
+	//				<serviceDebug includeExceptionDetailInFaults = "False" />
+
+ //               </ behavior >
+
+ //           </ serviceBehaviors >
+
+ //       </ behaviors >
+
+ //   </ system.serviceModel >
+    }
+}
+//< configuration >
+
+
+//    < appSettings >
+
+//        < add key = "aspnet:UseTaskFriendlySynchronizationContext" value = "true" />
+
+//    </ appSettings >
+
+//    < system.web >
+
+//        < compilation debug = "true" />
+
+//    </ system.web >
+
+//    < system.serviceModel >
+
+//        < services >
+
+//            < service name = "Kt10WcfServiceLibrary2.Service1" >
+
+//                < endpoint address = "" binding = "wsHttpBinding" contract = "Kt10WcfServiceLibrary2.ISecureService" >
+
+//                </ endpoint >
+
+//                < endpoint address = "mex" binding = "mexHttpBinding" contract = "IMetadataExchange" />
+
+//                < host >
+
+//                    < baseAddresses >
+
+//                        < add baseAddress = "http://localhost:8733/Design_Time_Addresses/Kt10WcfServiceLibrary2/Service1/" />
+
+//                    </ baseAddresses >
+
+//                </ host >
+
+//            </ service >
+
+//        </ services >
+
+//        < behaviors >
+
+//            < serviceBehaviors >
+
+//                < behavior >
+
+//                    < serviceMetadata httpGetEnabled = "True" httpsGetEnabled = "True" />
+
+//                    < serviceDebug includeExceptionDetailInFaults = "False" />
+
+//                </ behavior >
+
+//            </ serviceBehaviors >
+
+//        </ behaviors >
+
+//    </ system.serviceModel >
+
+//</ configuration >
+
+
+
+
+
+
+
+
+
